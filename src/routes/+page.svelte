0ffbc9734a9c3f9179c0a893e548b9557880902b
Nikaidou Haruki <nharuki@pm.me>
2026-04-20 22:55:46 +0900

    Update icon for Random Password button to use 'key-fill' variant for improved visual consistency.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@ import Icon from "@iconify/svelte";
 		class="neuo-btn h-36 w-36 rounded-xl flex flex-col items-center justify-center gap-2" 
 		href="/apps/random_password"
 		>
-      <Icon icon="mage:key" class="size-12" />
+      <Icon icon="mage:key-fill" class="size-12" />
 			<p class="text-sm font-semibold text-shadow-lg">Random Password</p>
 		</a>
 	</section>
