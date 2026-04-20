5969116c2617ae2f956f0ddbde2cd9576fb27a64
Nikaidou Haruki <nharuki@pm.me>
2026-04-20 22:34:50 +0900

    add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-import "./layout.css";
-import favicon from "$lib/assets/favicon.svg";
+    import "./layout.css";
+    import favicon from "$lib/assets/favicon.svg";
 
-let { children } = $props();
+    let { children } = $props();
 </script>
 
 <svelte:head
@@ -16,12 +16,26 @@ let { children } = $props();
         style="height: var(--header-height); box-shadow: var(--shadow-outer);"
     >
         <h1 class="m-0 text-2xl font-semibold text-foreground">
-            <a href="/">
-                Relief Toolbox
-            </a>
+            <a href="/"> Relief Toolbox </a>
         </h1>
     </header>
     <div class="flex flex-1 flex-col">
         {@render children()}
     </div>
+    <footer style="box-shadow: var(--shadow-outer);" class="p-4">
+        <p>
+            Open source collection of tools to help you with your daily tasks.
+            Built with SvelteKit and Rust.
+        </p>
+        <p>
+            <a
+                href="https://github.com/haruki-nikaidou/relief-toolbox"
+                class="text-primary underline underline-offset-1"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Github
+            </a>
+        </p>
+    </footer>
 </div>
