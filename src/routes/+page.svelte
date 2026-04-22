e0b17298adffe47eaa5afd1b58ca43785902d50b
Nikaidou Haruki <nharuki@pm.me>
2026-04-22 13:24:16 +0900

    add a new class

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,6 +31,11 @@ const appCollections: AppCollection[] = [
         appName: "SAT Solver",
         href: "/apps/sat_solver",
       },
+    ],
+  },
+  {
+    title: "Mathematical Tools",
+    apps: [
       {
         icon: "tabler:matrix",
         appName: "Gauss–Jordan Elimination",
@@ -42,13 +47,16 @@ const appCollections: AppCollection[] = [
 </script>
 
 <main class="container mx-auto px-4 py-8">
-  {#each appCollections as appCollection}
-	  <h2 class="text-xl font-semibold">{appCollection.title}</h2>
-		<section class="flex flex-row flex-wrap gap-4 justify-start mt-4 mb-8">
-			{#each appCollection.apps as app}
-				<AppIcon icon={app.icon} appName={app.appName} href={app.href} />
-			{/each}
-		</section>
-  {/each}
+    {#each appCollections as appCollection}
+        <h2 class="text-xl font-semibold">{appCollection.title}</h2>
+        <section class="flex flex-row flex-wrap gap-4 justify-start mt-4 mb-8">
+            {#each appCollection.apps as app}
+                <AppIcon
+                    icon={app.icon}
+                    appName={app.appName}
+                    href={app.href}
+                />
+            {/each}
+        </section>
+    {/each}
 </main>
-
