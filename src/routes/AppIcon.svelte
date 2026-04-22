0c7e2a9f0dfc36e6a3746767c589157ca73d74c0
Nikaidou Haruki <nharuki@pm.me>
2026-04-22 12:00:19 +0900

    fix styles in app icon

--- src/routes/AppIcon.svelte
+++ src/routes/AppIcon.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-import Icon from "@iconify/svelte";
+    import Icon from "@iconify/svelte";
 
-interface Props {
-  icon: string;
-  appName: string;
-  href: string;
-}
+    interface Props {
+        icon: string;
+        appName: string;
+        href: string;
+    }
 
-let { icon, appName, href }: Props = $props();
+    let { icon, appName, href }: Props = $props();
 </script>
 
-<a 
-	class="neuo-btn h-36 w-36 rounded-xl flex flex-col items-center justify-center gap-2" 
-	href={href}
+<a
+    class="neuo-btn h-36 w-36 rounded-xl flex flex-col items-center justify-center gap-2"
+    {href}
 >
-    <Icon icon={icon} class="size-12" />
-		<p class="text-sm font-semibold text-shadow-lg">{appName}</p>
+    <Icon {icon} class="size-12" />
+    <p class="text-sm font-semibold text-shadow-lg text-center">{appName}</p>
 </a>
